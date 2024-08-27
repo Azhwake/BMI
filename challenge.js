7075907e3eb1dfb6a53012db35d8877142d10287
Azhwake <azhwaq2002@gmail.com>
2024-08-27 15:56:41 +0300

    DONE

--- challenge.js
+++ challenge.js
@@ -1,2 +1,24 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculateBMI(weight, height) {
+    return weight / (height * height);
+}
+
+
+let aliBMI = calculateBMI(70, 1.75); 
+let mohamedBMI = calculateBMI(85, 1.80); 
+
+
+if (aliBMI > mohamedBMI) {
+    console.log("Ali's BMI is higher.");
+} else if (mohamedBMI > aliBMI) {
+    console.log("Mohamed's BMI is higher.");
+} else {
+    console.log("Ali and Mohamed have the same BMI.");
+}
+
+
+console.log(`Ali's BMI: ${aliBMI.toFixed(2)}`);
+console.log(`Mohamed's BMI: ${mohamedBMI.toFixed(2)}`);
+
